0dc981d4e5fec30675b33bb90a615a3ca298b562
rerjfjf <rerjfjf9@gmail.com>
2026-08-09 23:31:23 +0500

    Localize Ultra VIS lecture categories

---
+++ migrations/0006_russian_catalog_categories.sql
@@ -0,0 +1,9 @@
+UPDATE lectures SET category = 'Математика' WHERE id = 1;
+UPDATE lectures SET category = 'Физика' WHERE id = 2;
+UPDATE lectures SET category = 'Программирование' WHERE id IN (3, 4);
+UPDATE lectures SET category = 'Биология' WHERE id = 5;
+UPDATE lectures SET category = 'Химия' WHERE id = 6;
+UPDATE lectures SET category = 'Экономика' WHERE id = 7;
+UPDATE lectures SET category = 'История' WHERE id = 8;
+UPDATE lectures SET category = 'Языки' WHERE id = 9;
+UPDATE lectures SET category = 'Философия' WHERE id = 10;
